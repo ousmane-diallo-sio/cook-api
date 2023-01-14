b365a5dc7decced6df320374ed7e97a7721ea7fd
Aaldn <aaldn.dev@gmail.com>
2023-01-14 23:32:22 +0100

    feat: Implement login and logout

--- CookAPI/AuthenticationViewController.swift
+++ CookAPI/AuthenticationViewController.swift
@@ -6,18 +6,45 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class AuthenticationViewController: UIViewController {
+    
+    var handle: AuthStateDidChangeListenerHandle?
 
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registrationButton: UIButton!
+
+    @IBOutlet weak var mailErrorLabel: UILabel!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "Connexion"
+        self.mailErrorLabel.text = nil
+        self.passwordErrorLabel.text = nil
     }
-
+    
+    @IBAction func handleLogin(_ sender: UIButton) {
+        Authentication.signIn(email: self.mailTextField.text ?? "", password: self.passwordTextField.text ?? "")
+        /*
+        self.mailErrorLabel.text = "E-mail ou mot de passe invalide"
+        self.passwordErrorLabel.text = "E-mail ou mot de passe invalide"
+         */
+    }
+    
+     override func viewWillAppear(_ animated: Bool) {
+         handle = Auth.auth().addStateDidChangeListener { auth, user in
+             if user != nil {
+                 self.navigationController?.setViewControllers([SuccessAuthViewController()], animated: true)
+             }
+         }
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+         Auth.auth().removeStateDidChangeListener(handle!)
+     }
+     
 }
---
+++ CookAPI/Services/Authentication.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationViewController.swift
+//  CookAPI
+//
+//  Created by Aladdine Abdou on 14/01/2023.
+//
+
+import Foundation
+import FirebaseAuth
+
+class Authentication {
+    static let auth: Auth = Auth.auth()
+    
+    class func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { authResult, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    class func signOut() {
+        try? auth.signOut()
+    }
+    
+    class func getCurrentUser() -> User? {
+        return auth.currentUser
+    }
+}
